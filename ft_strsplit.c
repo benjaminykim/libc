60412b35fe750a4b75c54bae50257cc7ff2e900b
Benjamin Kim <bekim@e1z3r6p7.42.us.org>
2020-02-19 15:58:49 -0800

    strsplit fixed

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: bekim <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 20:13:57 by bekim             #+#    #+#             */
-/*   Updated: 2020/02/19 15:44:35 by bekim            ###   ########.fr       */
+/*   Updated: 2020/02/19 15:57:37 by bekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static int	str_len(char const *str, char c)
 	int		size;
 
 	size = 0;
-	while (*str != c)
+	while (*str != c && *str)
 	{
 		size++;
 		str++;
@@ -49,6 +49,8 @@ static char	*create_word(char const *str, int len)
 	char	*word;
 
 	word = ft_strnew(len);
+	if (word == NULL)
+		return (NULL);
 	ft_strncpy(word, str, len);
 	return (word);
 }
@@ -70,8 +72,10 @@ char		**ft_strsplit(char const *s, char c)
 		while (*s == c)
 			s++;
 		word = create_word(s, str_len(s, c));
+		if (word == NULL)
+			return (NULL);
 		ret[index] = word;
-		while (*s != c)
+		while (*s != c && *s)
 			s++;
 		index++;
 	}
