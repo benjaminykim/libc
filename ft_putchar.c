3f18d7914aed65a47034cee5fb2453aa43518a2a
Benjamin Kim <bekim@e1z3r6p7.42.us.org>
2020-02-19 15:06:27 -0800

    revert putchar unicode changes

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,13 +6,13 @@
 /*   By: bekim <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 20:10:13 by bekim             #+#    #+#             */
-/*   Updated: 2020/02/19 14:24:01 by bekim            ###   ########.fr       */
+/*   Updated: 2020/02/19 15:05:54 by bekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putchar(int c)
+void		ft_putchar(char c)
 {
 	ft_putchar_fd(c, 1);
 }
